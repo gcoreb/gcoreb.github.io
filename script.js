f04c9beacdff424ab08de9e33a4a380f4a34c78c
gcoreb <brybryc.music@gmail.com>
2015-08-30 11:44:43 -0700

    CSS beuty

--- script.js
+++ script.js
@@ -4,23 +4,36 @@
 $(document).ready(function () {
     var count = 0;
 $("#clickToAdd").click(function() {
-    count += 1;
-    swal({  title: "Events!",  
+    
+function alertIt() {
+    swal(
+      {  
+      title: "Events!",  
       text: "Please enter a task:",   
       type: "input",  
       showCancelButton: true, 
       closeOnConfirm: false,  
       animation: "slide-from-top",   
-      inputPlaceholder: "Task Name: i.e., Do the dishes" }, 
-    function(inputValue){  
-    if (inputValue === false) 
-        return false;     
-    if (inputValue === "") {   
-        swal.showInputError("You need to write something!");  
-        return false;  
-    }    
-    swal("Nice!", "Task " +  count + ": " + inputValue, "success");
-});
+      inputPlaceholder: "Task Name: i.e., Do the dishes" 
+      }, 
+      function(inputValue) {  
+      count += 1;
+      if (inputValue === false) 
+         return false;     
+            if (inputValue === "") {   
+                swal.showInputError("You need to write something!");  
+                return false;  
+                }    
+        swal("Nice!", "Task " +  count + ": " + inputValue, "success");
+
+    $('.list').append("<div class='item'>" + inputValue + "</div>");   
+
+        }
+    ); 
+    
+}; //end of alertIt function
+   alertIt();
+    
 })
 
 });
