650955fe290c7637090caae99fb7fc5e07acbf9a
gcoreb <brybryc.music@gmail.com>
2015-08-29 23:23:42 -0700

    Javascript file

---
+++ script.js
@@ -0,0 +1,6 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert
+*/
+$(document).ready(function () {
+swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
+}
