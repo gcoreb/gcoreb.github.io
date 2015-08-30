7badad6311e49030f4d4c773591b4f00504fc644
gcoreb <brybryc.music@gmail.com>
2015-08-30 10:42:16 -0700

    NEW UPDATE

--- script.js
+++ script.js
@@ -2,5 +2,25 @@
 /*global $, jQuery, alert
 */
 $(document).ready(function () {
-swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
+    var count = 0;
+$("#clickToAdd").click(function() {
+    count += 1;
+    swal({  title: "Events!",  
+      text: "Please enter a task:",   
+      type: "input",  
+      showCancelButton: true, 
+      closeOnConfirm: false,  
+      animation: "slide-from-top",   
+      inputPlaceholder: "Task Name: i.e., Do the dishes" }, 
+    function(inputValue){  
+    if (inputValue === false) 
+        return false;     
+    if (inputValue === "") {   
+        swal.showInputError("You need to write something!");  
+        return false;  
+    }    
+    swal("Nice!", "Task " +  count + ": " + inputValue, "success");
+});
 })
+
+});
